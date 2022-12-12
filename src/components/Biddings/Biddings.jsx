9277fd53feaf9c9c725d05e1dab30c1e994221ed
KasunShanaka <kshanaka10@gmail.com>
2022-12-13 00:57:49 +0530

    final commit

--- src/components/Biddings/Biddings.jsx
+++ src/components/Biddings/Biddings.jsx
@@ -10,24 +10,28 @@ const Biddings = () => {
   }, [badeVehicles]);
 
   return (
-    <div className={style.container}>
-      <div className={style.header}>Biddings</div>
-      <div className={style.content}>
-        <div className={style.card}>
-          <span className={style.image}>
-            <img
-              src="https://imgd-ct.aeplcdn.com/370x208/n/cw/ec/40432/scorpio-n-exterior-right-front-three-quarter-15.jpeg?isig=0&q=75"
-              alt=""
-            />
-          </span>
-          <span className={style.info}>
-            <div className={style.title}>Toyota</div>
-            <div className={style.bidValue}>19239000.00 LKR</div>
-          </span>
-        </div>
+    <>
+      <div className={style.container}>
+        <div className={style.header}>Biddings</div>
+        {badeVehicles.map((item, index) => (
+          <div className={style.content}>
+            <div className={style.card}>
+              <span className={style.image}>
+                <img
+                  src="https://imgd-ct.aeplcdn.com/370x208/n/cw/ec/40432/scorpio-n-exterior-right-front-three-quarter-15.jpeg?isig=0&q=75"
+                  alt=""
+                />
+              </span>
+              <span className={style.info}>
+                <div className={style.title}>Toyota</div>
+                <div className={style.bidValue}>19239000.00 LKR</div>
+              </span>
+            </div>
+          </div>
+        ))}
       </div>
       <div className={style.footer}>Total: 236438 LKR</div>
-    </div>
+    </>
   );
 };
 
